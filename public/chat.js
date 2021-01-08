b4942a4392533259f28d2ad21bdfcfc29c825f02
ajayrwarrier <ajayrwarrier@gmail.com>
2021-01-08 16:21:26 +0530

    Fixed issue with Feedback.
    Fixed issue with Feedback.

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-let socket = io.connect("http://localhost:4000");
+let socket = io();
 let divVideoChatLobby = document.getElementById("video-chat-lobby");
 let divVideoChat = document.getElementById("video-chat-room");
 let joinButton = document.getElementById("join");
@@ -149,6 +149,8 @@ socket.on("ready", function () {
     rtcPeerConnection.ontrack = OnTrackFunction;
     rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
     rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+
+
     rtcPeerConnection
       .createOffer()
       .then((offer) => {
